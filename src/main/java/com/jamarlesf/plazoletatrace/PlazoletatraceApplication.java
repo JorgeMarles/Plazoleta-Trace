090bf343e026a8cbbcf3943e91c627c6d15f3ad6
Jorge Marles <jamarlesf@gmail.com>
2026-07-14 14:34:20 -0500

    feat: initial commit for plazoletatrace with JWT and OpenAPI skeleton

---
+++ src/main/java/com/jamarlesf/plazoletatrace/PlazoletatraceApplication.java
@@ -0,0 +1,13 @@
+package com.jamarlesf.plazoletatrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazoletatraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazoletatraceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jamarlesf/plazoletatrace/infrastructure/configuration/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.jamarlesf.plazoletatrace.infrastructure.configuration;
+
+import com.jamarlesf.plazoletatrace.infrastructure.security.filter.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
